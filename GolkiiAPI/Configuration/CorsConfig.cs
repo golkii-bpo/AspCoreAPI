c9ecda49469f581035702edce11b88b4b662a2ec
Sergio Lopez <serlopsal_199@yahoo.es>
2019-05-10 14:08:43 -0600

    Full Busqueda Completa Persona

---
+++ GolkiiAPI/Configuration/CorsConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GolkiiAPI.Configuration
+{
+    public static class CorsConfig
+    {
+
+        public static void AddCorsPolicy(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                builder =>
+                {
+                    builder
+                       // .WithOrigins("http://localhost:3000","http://192.168.1.*")
+                        .AllowAnyOrigin()
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .WithMethods("OPTIONS","GET","POST")
+                        .AllowAnyHeader();
+                });
+            });
+        }
+
+        internal static void UseCors(IApplicationBuilder app)
+        {
+            app.UseCors();
+        }
+    }
+}
